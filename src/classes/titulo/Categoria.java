9a345270c14c24bd4ad434c1d38bdb648b2c28c4
brunomatheuss <47148367+brunomatheuss@users.noreply.github.com>
2019-09-19 10:10:35 -0300

    Criação da classe Categoria juntamente com seus atributos, construtor, getters e setters.

---
+++ src/classes/titulo/Categoria.java
@@ -0,0 +1,59 @@
+package classes.titulo;
+import java.util.ArrayList;
+public class Categoria {
+    // ATRIBUTOS
+    private String nome;
+    private ArrayList<Titulo> listaTitulosNota1;
+    private ArrayList<Titulo> listaTitulosNota2;
+    private ArrayList<Titulo> listaTitulosNota3;
+    private ArrayList<Titulo> listaTitulosNota4;
+    private ArrayList<Titulo> listaTitulosNota5;
+    
+    // CONSTRUTOR
+    public Categoria(String nome) {
+        this.nome = nome;
+        this.listaTitulosNota1 = new ArrayList();
+        this.listaTitulosNota2 = new ArrayList();
+        this.listaTitulosNota3 = new ArrayList();
+        this.listaTitulosNota4 = new ArrayList();
+        this.listaTitulosNota5 = new ArrayList();
+    }
+    
+    // GETTERS E SETTERS
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public ArrayList<Titulo> getListaTitulosNota1() {
+        return listaTitulosNota1;
+    }
+    public void setListaTitulosNota1(ArrayList<Titulo> listaTitulosNota1) {
+        this.listaTitulosNota1 = listaTitulosNota1;
+    }
+    public ArrayList<Titulo> getListaTitulosNota2() {
+        return listaTitulosNota2;
+    }
+    public void setListaTitulosNota2(ArrayList<Titulo> listaTitulosNota2) {
+        this.listaTitulosNota2 = listaTitulosNota2;
+    }
+    public ArrayList<Titulo> getListaTitulosNota3() {
+        return listaTitulosNota3;
+    }
+    public void setListaTitulosNota3(ArrayList<Titulo> listaTitulosNota3) {
+        this.listaTitulosNota3 = listaTitulosNota3;
+    }
+    public ArrayList<Titulo> getListaTitulosNota4() {
+        return listaTitulosNota4;
+    }
+    public void setListaTitulosNota4(ArrayList<Titulo> listaTitulosNota4) {
+        this.listaTitulosNota4 = listaTitulosNota4;
+    }
+    public ArrayList<Titulo> getListaTitulosNota5() {
+        return listaTitulosNota5;
+    }
+    public void setListaTitulosNota5(ArrayList<Titulo> listaTitulosNota5) {
+        this.listaTitulosNota5 = listaTitulosNota5;
+    }
+}
